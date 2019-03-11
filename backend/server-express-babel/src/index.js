00dc37d921603adaadaa6523a2ec13805ae03057
Osman Cea <osman.cea@gmail.com>
2019-03-11 16:55:42 -0300

    refactor: move server-express to server-express-babel and add package for endpoints

--- backend/server-express-babel/src/index.js
+++ backend/server-express-babel/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+const endpoint = require('@js-fws/endpoints');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -6,6 +7,12 @@ const app = express();
 app.get('/', (req, res) => {
   res.send(`Hello kit`);
 });
+
+app.get(endpoint.counter, (req, res) => {
+  res.set('Content-Type', 'application/json');
+
+  res.send({ counters: [] });
+})
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
---
+++ backend/server-express/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const endpoint = require('@js-fws/endpoints');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`Hello kitten`);
+});
+
+app.get(endpoint.counter, (req, res) => {
+  res.set('Content-Type', 'application/json');
+
+  res.send({ counters: [] });
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
